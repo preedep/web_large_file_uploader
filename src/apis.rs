ab021b12e9ccf134c747ab6f5cfdec4c295c1279
Preedee Ponchevin <nickmsft@mac.com>
2024-01-08 11:12:15 +0700

    - Add sqlite - Add web static

--- src/apis.rs
+++ src/apis.rs
@@ -1,4 +1,8 @@
-use actix_web::{HttpResponse, Responder};
+use actix_web::{HttpResponse, Responder, web};
+use azure_identity::DefaultAzureCredential;
+use azure_core::auth::TokenCredential;
+
+use r2d2_sqlite::SqliteConnectionManager;
 use serde::{Deserialize, Serialize};
 use tracing_attributes::instrument;
 
@@ -12,10 +16,30 @@ pub struct StartUploadRequest {
     pub file_hash: String,
 }
 
-pub struct UploadResponse {}
+pub struct UploadResponse {
+    pub upload_id : String,
+    pub chunk_size : Option<u64>,
+}
+type DbPool = r2d2::Pool<SqliteConnectionManager>;
 
 #[instrument]
-pub async fn start_upload(req: actix_web::web::Json<StartUploadRequest>) -> impl Responder {
+pub async fn start_upload(
+    pool: web::Data<DbPool>,
+    req: web::Json<StartUploadRequest>) -> impl Responder {
+
+    let credential = DefaultAzureCredential::default();
+    let response = credential
+        .get_token(&["https://management.azure.com/.default"])
+        .await;
+
+    if let Err(err) = response {
+        println!("Failed to get token: {:?}", err);
+        return HttpResponse::InternalServerError().body("Failed to get token");
+    }
+    let token = response.unwrap();
+    let upload_id = uuid::Uuid::new_v4().to_string();
+
+
     println!("start_upload: {:?}", req);
     HttpResponse::Ok().body("start_upload")
 }
